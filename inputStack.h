60623cae82283e665b967e43d3a7340a9ed219e0
marcusCarpenter97 <marcus.carpenter97@yahoo.co.uk>
2019-06-23 17:00:48 +0100

    Added header guards.

--- inputStack.h
+++ inputStack.h
@@ -1,5 +1,16 @@
+#ifndef INPUTSTACK_H
+#define INPUTSTACK_H
+
 #define MAX_SIZE 5 
 #define EMPTY -1
 
 const char *input_stack[MAX_SIZE];
 int top = EMPTY;
+
+int is_empty();
+int is_full();
+const char* peek();
+const char* pop();
+int push(const char* item);
+
+#endif /* INPUTSTACK_H */
