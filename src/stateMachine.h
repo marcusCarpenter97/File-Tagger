be746e6a22106aa4d4e006ec40fa47fa5bc6306c
marcusCarpenter97 <marcus.carpenter97@yahoo.co.uk>
2019-06-30 15:14:49 +0100

    File and directory paths can now be validated properly.

--- src/stateMachine.h
+++ src/stateMachine.h
@@ -2,10 +2,12 @@
 #define STATEMACHINE_H
 
 int verify_input(void);
-//int num_of_transitions, char* transitions[]
 int move_to_next_state(unsigned int cur_state, unsigned int ret_code);
 
-int waiting_for_input_state(const char* s);
+/* Helper function */
+int check_path();
+
+int waiting_for_input_state(const char* s); /* Depreceted. */
 int select_option_state(const char* s);
 int add_tag_state(const char* s);
 int add_files_selected_state(const char* s);
@@ -13,6 +15,8 @@ int add_tags_selected_state(const char* s);
 int add_tags_state(const char* s);
 
 extern int (* state[])(const char* s); /* Array of function pointers where all functions take a cons char* as a parameter. */
+
+enum path_types {directory, file, invalid_path};
 
 enum state_codes {waiting_for_input, select_option, add_tag, add_files_selected, add_tags_selected, add_tags};
 
